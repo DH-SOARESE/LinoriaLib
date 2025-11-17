d1b6e3141783cb1b9c1a10a44fbeec3cdebc12a4
DH-SOARESE <dhsoares0@gmail.com>
2025-11-17 09:47:25 -0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2337,7 +2337,7 @@ function BaseAddonsFuncs:AddDropdown(Idx, Info)
             ImageColor3 = Library.AccentColor;
             Position = UDim2.new(1, -16, 0.5, 0);
             Size = UDim2.new(0, 12, 0, 12);
-            Image = 'http://www.roblox.com/asset/?id=6282522798';
+            Image = 'rbxassetid://118256101676935';
             ZIndex = 8;
             Parent = DropdownInner;
         });
