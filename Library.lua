df3d962926e2814885f61a6ce4d244c2d3f22e71
DH-SOARESE <dhsoares0@gmail.com>
2025-11-21 17:54:56 -0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -203,7 +203,7 @@ local CursorImage = Instance.new("ImageLabel")
 CursorImage.Size = UDim2.fromOffset(Library.CursorSize, Library.CursorSize)
 CursorImage.AnchorPoint = Vector2.new(0.5, 0.5)
 CursorImage.BackgroundTransparency = 1
-CursorImage.Image = "rbxassetid://" .. tostring(Library.CursorImage
+CursorImage.Image = "rbxassetid://" .. tostring(Library.CursorImage)
 CursorImage.ZIndex = 0
 CursorImage.Parent = LinoriaCursor
 
